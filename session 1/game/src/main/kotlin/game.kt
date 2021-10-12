f0ad18940810083b0e4062707ef0060f1c4a37d6
HajarLamriSaifi <56121110+HajarLamriSaifi@users.noreply.github.com>
2021-10-12 10:05:42 +0200

    dev3
    game rock paper scissors

--- session 1/game/src/main/kotlin/game.kt
+++ session 1/game/src/main/kotlin/game.kt
@@ -1,7 +1,36 @@
-val gameWeapon = arrayOf("rock", "paper", "scissors")
-val randomWeapon = gameWeapon.random()
 
-fun main (){
-    val gameWeapon = arrayOf("rock", "paper", "scissors")
-    val randomWeapon = gameWeapon.random()
+fun main(){
+    val options = arrayOf("rock", "paper", "scissors")
+    val computerChoice = options.random()
+    val userChoice = getUserChoice(options)
+
+    println("computer picked $computerChoice")
+    println("user picked $userChoice")
+    determineWinner(computerChoice, userChoice)
+}
+
+fun getUserChoice(options: Array<String>): String? {
+    var userChoice: String?
+    do{
+        println("make your choice: rock - paper - scissors")
+        userChoice = readLine()
+    } while (!options.contains(userChoice))
+    return userChoice
+}
+
+fun determineWinner(computerChoice: String, userChoice: String?){
+    if (computerChoice == userChoice){
+        println("it's a draw")
+        return
+    }
+
+    val winMessage = "you've won"
+    val loseMessage = "you've lost"
+
+    when (computerChoice) {
+        "rock" -> if (userChoice == "paper") println(winMessage) else println(loseMessage)
+        "paper" -> if (userChoice == "scissors") println(winMessage) else println(loseMessage)
+        "scissors" -> if (userChoice == "rock") println(winMessage) else println(loseMessage)
+
+    }
 }
