012b23a9e8c2e38476f33318c55626b3af31b171
HajarLamriSaifi <56121110+HajarLamriSaifi@users.noreply.github.com>
2021-10-12 12:22:08 +0200

    challenge-two
    challenge-two

--- session2/src/main/kotlin/challengeOne.kt
+++ session2/src/main/kotlin/challengeOne.kt
@@ -10,8 +10,25 @@ fun challengeOne(){
     if ( playerAnswer != null ){
         if (playerAnswer.contains(correctanswer)){
             println("you are correct")
+            challengeTwo()
         }else {
             println("you are wrong")
         }
     }
 }
+
+
+fun challengeTwo(){
+    val userGeuss = readLine()
+    val diceResult = (1..6)random()
+
+    //TODO: check  == problem for strings
+    if (diceResult >= 4){
+        winner()
+    }
+    else{
+        gameover()
+    }
+}
+
+fun winner()
