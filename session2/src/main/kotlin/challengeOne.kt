680f9237023729b5b29cb55f44a526a165f4964f
HajarLamriSaifi <56121110+HajarLamriSaifi@users.noreply.github.com>
2021-10-12 11:45:13 +0200

    challengeone
    geuss my name

--- session2/src/main/kotlin/challengeOne.kt
+++ session2/src/main/kotlin/challengeOne.kt
@@ -1,3 +1,17 @@
 fun main(){
-    println("hello")
+    println("Geuss my name ")
+    challengeOne()
+}
+
+fun challengeOne(){
+    val correctanswer = "hajar"
+    val playerAnswer = readLine();
+
+    if ( playerAnswer != null ){
+        if (playerAnswer.contains(correctanswer)){
+            println("you are correct")
+        }else {
+            println("you are wrong")
+        }
+    }
 }
